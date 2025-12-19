76c558fafdd8bb291dc1581385f1ec0bbd39a5ef
pandascrush <velayuthamsiva55@gmail.com>
2025-12-19 10:48:54 +0530

    feat:build created afetr the auth middleware added

--- dist/routes/event.routes.js
+++ dist/routes/event.routes.js
@@ -4,6 +4,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
+const authMiddleware_1 = require("../middlewares/authMiddleware");
 const event_controller_1 = require("../controllers/event.controller");
 const fileUpload_1 = __importDefault(require("../middlewares/fileUpload"));
 const validate_1 = require("../utils/validate");
@@ -13,27 +14,27 @@ const router = (0, express_1.Router)();
  * @route GET /api/v1/organizations/:orgId/events
  * @desc  Get all events of a specific organization
  */
-router.get("/organizations/:orgId/events", (0, validate_1.validate)(event_validation_1.eventValidation.getAll), event_controller_1.EventController.getOrgEvents);
+router.get("/organizations/:orgId/events", authMiddleware_1.authMiddleware, (0, validate_1.validate)(event_validation_1.eventValidation.getAll), event_controller_1.EventController.getOrgEvents);
 /**
  * @route GET /api/v1/organizations/:orgId/events/:eventId
  * @desc  Get a single event under an organization
  */
-router.get("/organizations/:orgId/events/:eventId", (0, validate_1.validate)(event_validation_1.eventValidation.getSingle), event_controller_1.EventController.getEventById);
+router.get("/organizations/:orgId/events/:eventId", authMiddleware_1.authMiddleware, (0, validate_1.validate)(event_validation_1.eventValidation.getSingle), event_controller_1.EventController.getEventById);
 /**
  * @route POST /api/v1/organizations/:orgId/events
  * @desc  Create a new event (with image upload)
  */
-router.post("/organizations/:orgId/events", (0, validate_1.validate)(event_validation_1.eventValidation.create), fileUpload_1.default.single("image"), event_controller_1.EventController.createEvent);
+router.post("/organizations/:orgId/events", authMiddleware_1.authMiddleware, (0, validate_1.validate)(event_validation_1.eventValidation.create), fileUpload_1.default.single("image"), event_controller_1.EventController.createEvent);
 /**
  * @route PUT /api/v1/organizations/:orgId/events/:eventId
  * @desc  Update an event (with optional image upload)
  */
-router.put("/organizations/:orgId/events/:eventId", (0, validate_1.validate)(event_validation_1.eventValidation.update), fileUpload_1.default.single("image"), event_controller_1.EventController.updateEvent);
+router.put("/organizations/:orgId/events/:eventId", authMiddleware_1.authMiddleware, (0, validate_1.validate)(event_validation_1.eventValidation.update), fileUpload_1.default.single("image"), event_controller_1.EventController.updateEvent);
 /**
  * @route DELETE /api/v1/organizations/:orgId/events/:eventId
  * @desc  Delete an event
  */
-router.delete("/organizations/:orgId/events/:eventId", (0, validate_1.validate)(event_validation_1.eventValidation.deleteEvent), event_controller_1.EventController.deleteEvent);
+router.delete("/organizations/:orgId/events/:eventId", authMiddleware_1.authMiddleware, (0, validate_1.validate)(event_validation_1.eventValidation.deleteEvent), event_controller_1.EventController.deleteEvent);
 /* ----------------------- PUBLIC EVENT ROUTES ----------------------- */
 /**
  * @route GET /api/v1/events
